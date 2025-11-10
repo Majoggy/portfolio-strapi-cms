5d524a311a50434be9288e392e013da4c8757efe
Christian Baker <cbaker87@gmail.com>
2025-11-10 16:05:02 +0000

    Initial setup, graphql, adding all collections

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default {
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,      
+      depthLimit: 7,
+      amountLimit: 100,
+      apolloServer: {
+        introspection: true,  
+      },
+    },
+  },
+};
